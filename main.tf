ba35d8709d7c2fccbdbb658318d88e652cb74151
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2024-11-27 17:41:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,37 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
+# Specify the provider
+provider "azurerm" {
+  features {} # Enables the provider; mandatory but can be empty.
+}
+
+# Define variables (optional)
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  default     = "my-resource-group"
+}
+
+variable "location" {
+  description = "Azure Region where the Resource Group will be created"
+  default     = "East US"
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = "Development"
+    Owner       = "YourName"
   }
 }
 
-provider "azurerm" {
-  features {}
+# Output the Resource Group details
+output "resource_group_name" {
+  description = "The name of the created Resource Group"
+  value       = azurerm_resource_group.example.name
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+output "resource_group_location" {
+  description = "The location of the created Resource Group"
+  value       = azurerm_resource_group.example.location
 }
