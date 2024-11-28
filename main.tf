ead334818a853361a6578de03107bebdd9f41988
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2024-11-28 10:04:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,83 @@
-# Specify the provider
-provider "azurerm" {
-  features {} # Enables the provider; mandatory but can be empty.
+# Specify the AWS provider and region
+provider "aws" {
+  region = "us-east-1"
 }
 
-# Define variables (optional)
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  default     = "my-resource-group"
-}
-
-variable "location" {
-  description = "Azure Region where the Resource Group will be created"
-  default     = "East US"
-}
-
-# Create the resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-
+# Create the VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags = {
-    Environment = "Development"
-    Owner       = "YourName"
+    Name = "MyVPC"
   }
 }
 
-# Output the Resource Group details
-output "resource_group_name" {
-  description = "The name of the created Resource Group"
-  value       = azurerm_resource_group.example.name
+# Create a subnet in the VPC
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MySubnet"
+  }
 }
 
-output "resource_group_location" {
-  description = "The location of the created Resource Group"
-  value       = azurerm_resource_group.example.location
+# Create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+# Create a route table to route traffic to the internet
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a route to allow internet traffic
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create a basic security group
+resource "aws_security_group" "allow_all" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
+# Outputs
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.subnet_1.id
 }
